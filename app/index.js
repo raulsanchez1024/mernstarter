e9ff3ab321a9a4a6e8793806cfc97dfc091ea6f1
Raul Sanchez <raulsanchez1024@gmail.com>
2019-08-14 23:24:44 -0600

    Version 0.1

--- app/index.js
+++ app/index.js
@@ -1,1 +1,18 @@
-// what
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
+
+module.exports = class extends Generator {
+  method1() {
+    this.log("method 1 just ran");
+  }
+
+  propmpting() {
+    this.log(
+      yosay(`Welcome to the exquisite ${chalk.red(`MERN STARTER`)} generator!`)
+    );
+  }
+  installing() {
+    this.installDependencies({ npm: true, bower: false, yarn: false });
+  }
+};
